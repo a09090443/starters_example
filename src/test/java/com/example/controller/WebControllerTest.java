fbfcefe3f65592cfa06e3d5d039da90a83f05d68
Zipe <zipe.daden@gmail.com>
2023-05-02 22:29:01 +0800

    Add controller unit test.

--- src/test/java/com/example/controller/WebControllerTest.java
+++ src/test/java/com/example/controller/WebControllerTest.java
@@ -4,7 +4,9 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.example.base.TestBase;
 import lombok.extern.slf4j.Slf4j;
@@ -15,14 +17,17 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @Slf4j
 @AutoConfigureMockMvc
-public class RestSecurityTest extends TestBase {
+public class WebControllerTest extends TestBase {
 
   @Autowired
   private MockMvc mockMvc;
 
   @Test
-  public void sayHelloTest() throws Exception {
-    this.mockMvc.perform(get("/rest/sayHello?name=John")).andDo(print()).andExpect(status().isOk())
-        .andExpect(content().string(containsString("Hello,  John!")));
+  public void testThymeleaf() throws Exception {
+    this.mockMvc.perform(get("/thymeleaf")).andExpect(status().isOk());
+  }
+  @Test
+  public void testJsp() throws Exception {
+    this.mockMvc.perform(get("/jsp")).andExpect(status().isOk());
   }
 }
