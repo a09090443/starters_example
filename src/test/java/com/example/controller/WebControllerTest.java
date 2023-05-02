878633ac047f29c5ca6cb680c32a00e39a94a647
Zipe <zipe.daden@gmail.com>
2023-05-02 22:43:20 +0800

    Add controller unit test.

--- src/test/java/com/example/controller/WebControllerTest.java
+++ src/test/java/com/example/controller/WebControllerTest.java
@@ -1,12 +1,7 @@
 package com.example.controller;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.example.base.TestBase;
 import lombok.extern.slf4j.Slf4j;
