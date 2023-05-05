c08b1a0ca31cf6681d2c3f3fb0c127c2aa00ed68
Gary.Tsai <gary.tsai@hytechc.com>
2023-05-05 10:07:23 +0800

    1. Modify webservice test code.

--- src/test/java/com/example/webservice/ExampleWebServiceTest.java
+++ src/test/java/com/example/webservice/ExampleWebServiceTest.java
@@ -3,28 +3,22 @@ package com.example.webservice;
 import com.zipe.model.User;
 import com.zipe.util.WebServiceClientUtil;
 import javax.xml.namespace.QName;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author : Gary Tsai
  * @created : @Date 2021/5/4 下午 02:55
  **/
+@Slf4j
 public class ExampleWebServiceTest {
     private final String WEB_SERVICE_URL = "http://localhost:8080/example/webservice/example?wsdl";
-    public static void main(String[] args) throws Exception {
-        try {
-            ExampleWebServiceTest exampleWebServiceTest = new ExampleWebServiceTest();
-            exampleWebServiceTest.getUserByProxy();
-            exampleWebServiceTest.getUserByClientUtil();
-            exampleWebServiceTest.getUserNameByDynamicClient();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    private void getUserByProxy(){
+    @Test
+    void getUserByProxy(){
         try {
             // 代理工廠
             JaxWsProxyFactoryBean jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
@@ -38,20 +32,22 @@ public class ExampleWebServiceTest {
             String userId = "02";
             // 調用代理接口的方法調用並返回結果
             User result = us.getUser(userId);
-//            Map<String, User> userMaps= us.getAllUserData();
-            System.out.println("返回結果:" + result);
+            log.info("返回結果:{}", result);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void getUserByClientUtil() throws Exception {
+    @Test
+    void getUserByClientUtil() throws Exception {
         WebServiceClientUtil clientUtil = new WebServiceClientUtil(
             WEB_SERVICE_URL, "getUser", new Object[]{"01"});
         Object[] result = clientUtil.invoke();
-        System.out.println("返回結果:" + result[0]);
+        log.info("返回結果:{}", result[0]);
     }
-    private void getUserNameByDynamicClient() throws Exception {
+
+    @Test
+    void getUserNameByDynamicClient() throws Exception {
         try {
             // 代理工廠
             JaxWsDynamicClientFactory jaxWsDynamicClientFactory = JaxWsDynamicClientFactory.newInstance();
@@ -60,8 +56,7 @@ public class ExampleWebServiceTest {
             // 設置targetNamespace 和 methodName
             QName qname = new QName("http://service.example.com", "getUserName");
             Object[] result = client.invoke(qname, "01");
-
-            System.out.println("返回結果:" + result[0]);
+            log.info("返回結果:{}", result[0]);
         } catch (Exception e) {
             e.printStackTrace();
         }
