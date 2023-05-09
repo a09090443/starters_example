834cdac7f51d013bad4186ab2e980d6ba288d612
Zipe <zipe.daden@gmail.com>
2023-05-09 22:18:07 +0800

    Adjust database setting.

--- src/main/resources/init/data.sql
+++ src/main/resources/init/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO UserMain (Name) VALUES('Tom');
-INSERT INTO UserMain (Name) VALUES('Jen');
-INSERT INTO UserMain (Name) VALUES('Andy');
+INSERT INTO user_main (Name) VALUES('Tom');
+INSERT INTO user_main (Name) VALUES('Jen');
+INSERT INTO user_main (Name) VALUES('Andy');
 
-INSERT INTO UserDetail (Name, Gender) VALUES('Tom', 'M');
-INSERT INTO UserDetail (Name, Gender) VALUES('Jen', 'F');
-INSERT INTO UserDetail (Name, Gender) VALUES('Andy', 'M');
+INSERT INTO user_detail (Name, Gender) VALUES('Tom', 'M');
+INSERT INTO user_detail (Name, Gender) VALUES('Jen', 'F');
+INSERT INTO user_detail (Name, Gender) VALUES('Andy', 'M');
